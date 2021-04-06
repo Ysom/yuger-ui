4d3be1bb9119b4f197a2ec7a7001177bf026693e
Ysom <1083688285@qq.com>
2021-04-06 17:35:52 +0800

    Initial: react和typescript开发配置

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Yuger-UI：基于 React 和 TypeScript 开发的 UI 库.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
