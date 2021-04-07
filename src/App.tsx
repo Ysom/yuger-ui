869bc3fd9958b975a5bb2bd784bbc6b73f3f5f3a
Ysom <1083688285@qq.com>
2021-04-07 17:27:44 +0800

    update: 按钮组件添加样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Button, {ButtonSize, ButtonType} from './components/Button/button'
+import Button, { ButtonSize, ButtonType } from "./components/Button/button";
 import "./App.scss";
 
 function App() {
@@ -6,8 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Yuger-UI：基于 React 和 TypeScript 开发的 UI 库.</p>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>按钮</Button>
-        <Button btnType={ButtonType.Link} href="www.baidu.com">Link Button</Button>
+        <div>
+          <Button disabled>Default</Button>
+          <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+            Primary Small
+          </Button>
+          <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>
+            Danger Large
+          </Button>
+          <Button btnType={ButtonType.Link} href="www.baidu.com">
+            Link
+          </Button>
+          <Button btnType={ButtonType.Link} disabled href="www.baidu.com">
+            Link Disabled
+          </Button>
+        </div>
       </header>
     </div>
   );
