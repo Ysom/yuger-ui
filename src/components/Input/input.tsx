0d115f18cfb914f5bb1ab79a0bdd5b8a7888e23b
Ysom <1083688285@qq.com>
2021-08-01 19:03:31 +0800

    add: 增加 Input 组件内容

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,5 +1,7 @@
 import React, { InputHTMLAttributes, ReactElement, FC } from "react";
+import classNames from 'classnames';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import Icon from '../Icon/icon'
 
 type InputSize = 'lg' | 'sm';
 
@@ -12,4 +14,37 @@ export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size
   append?: string | ReactElement;
 };
 
-export const Input: FC<InputProps> = (props) => {};
+export const Input: FC<InputProps> = (props) => {
+  const {
+    disabled,
+    size,
+    icon,
+    prepend,
+    append,
+    style,
+    ...restProps
+  } = props;
+
+  const cnames = classNames('yuger-input-wrapper', {
+    [`input-size-${size}`]: size,
+    'is-disabled': disabled,
+    'input-group': prepend || append,
+    'input-group-append': !!append,
+    'input-group-prepend': !!prepend
+  });
+
+  return (
+    <div className={cnames} style={style}>
+      {prepend && <div className="yuger-input-group-prepend">{prepend}</div>}
+      {icon && <div className="icon-wrapper"><Icon icon={icon} title={`title-${icon}`}/></div>}
+      <input 
+        className="yuger-input-inner"
+        disabled={disabled}
+        {...restProps}
+      />
+      {append && <div className="yuger-input-group-append">{append}</div>}
+    </div>
+  );
+};
+
+export default Input;
